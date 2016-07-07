d055020585395f9662b898fd02955872c0e3efd2
patillades <gorkamunya@hotmail.com>
2016-07-07 15:39:10 +0200

    refactor so the state is handled by redux; not working yet

--- lib/components/jsx/TotalTimer.jsx
+++ lib/components/jsx/TotalTimer.jsx
@@ -1,42 +1,80 @@
 const React = require('react');
+const redux = require('redux');
 
 const date = require('./../../date');
 
 const Timer = require('./Timer');
 const PlayButton = require('./PlayButton');
 
+const initialState = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  start: null
+};
+
+// actions
+const PLAY = 'play';
+const TICK = 'tick';
+
+// action creators
+function play() {
+  return {
+    type: PLAY
+  };
+}
+
+function tick() {
+  return {
+    type: TICK
+  };
+}
+
+// reducers
+function stopwatch(state = initialState, action) {
+  switch (action.type) {
+    case PLAY:
+      return Object.assign({}, state, {
+        start: Date.now()
+      });
+
+    case TICK:
+      return Object.assign({}, state, date.diff(state.start, Date.now()));
+
+    default:
+      return state;
+  }
+}
+
 class TotalTimer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.clickPlayHandler = this.clickPlayHandler.bind(this);
+    // store
+    this.store = redux.createStore(stopwatch);
 
-    this.state = {
-      hours: 0,
-      minutes: 0,
-      seconds: 0
-    };
+    this.clickPlayHandler = this.clickPlayHandler.bind(this);
   }
 
   clickPlayHandler(e) {
-    this.start = Date.now();
+    this.store.dispatch(play());
 
     setInterval(this.updateTimer.bind(this), 100);
   }
 
   updateTimer() {
-    this.setState(
-      date.diff(this.start, Date.now())
-    );
+    this.store.dispatch(tick());
   }
 
   render() {
+    const state = this.store.getState();
+
     return (
       <div>
         <Timer
-          hours={this.state.hours}
-          minutes={this.state.minutes}
-          seconds={this.state.seconds}
+          hours={state.hours}
+          minutes={state.minutes}
+          seconds={state.seconds}
         />
 
         <PlayButton clickHandler={this.clickPlayHandler} />
