eb23918e52f7871107ee5f0fd06a6ac56f05ab9e
patillades <gorkamunya@hotmail.com>
2016-07-24 11:52:07 +0200

    added first simple tests

---
+++ tests/dateTest.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import { diff, twoDigits } from '../lib/utils/date';
+
+describe('date utils', function () {
+  describe('twoDigits function', function () {
+    it('should return any two-digit number as a string', function () {
+      const result = twoDigits(11);
+
+      expect(result).toBeA('string');
+      expect(result).toBe('11');
+    });
+
+    it('should add a leading 0 to any one-digit number', function () {
+      const result = twoDigits(1);
+
+      expect(result).toBeA('string');
+      expect(result).toBe('01');
+    });
+  });
+});
---
+++ tests/twoTimingDigitsTest.jsx
@@ -0,0 +1,23 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import TwoTimingDigits from '../lib/components/TwoTimingDigits.jsx';
+
+function setup() {
+  const props = { value: 0 };
+
+  const renderer = TestUtils.createRenderer();
+
+  renderer.render(<TwoTimingDigits {...props} />);
+
+  return renderer.getRenderOutput();
+}
+
+describe('TwoTimingDigits', function () {
+  it('should render element', function () {
+    const element = setup();
+
+    expect(element.type).toBe('span');
+    expect(element.props.children).toBe('00');
+  });
+});
