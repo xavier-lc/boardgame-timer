dcaa6f495d98800c41ab55664255bb1d279cce24
patillades <gorkamunya@hotmail.com>
2016-08-09 13:22:10 +0200

    added time function

--- tests/utils/dateTest.js
+++ tests/utils/dateTest.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import { diff, twoDigits } from './../../lib/utils/date';
+import { diff, timeToMs, twoDigits } from './../../lib/utils/date';
 
 describe('date utils', function () {
   describe('twoDigits function', function () {
@@ -15,6 +15,26 @@ describe('date utils', function () {
 
       expect(result).toBeA('string');
       expect(result).toBe('01');
+    });
+  });
+
+  describe('timeToMs function', function () {
+    it('should be the reverse of the diff function', function () {
+      const time = {
+        days: 1,
+        hours: 2,
+        minutes: 3,
+        seconds: 4,
+        milliseconds: 5,
+      };
+
+      const difference = diff(0, timeToMs(time));
+
+      expect(difference.days).toBe(time.days);
+      expect(difference.hours).toBe(time.hours);
+      expect(difference.minutes).toBe(time.minutes);
+      expect(difference.seconds).toBe(time.seconds);
+      expect(difference.milliseconds).toBe(time.milliseconds);
     });
   });
 });
