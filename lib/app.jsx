45ac251ae693c48cf8ebd39aa840939d4b6ca143
patillades <gorkamunya@hotmail.com>
2016-08-18 11:05:57 +0200

    using react router for navigation

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Router from 'react-router/lib/Router';
+import Route from 'react-router/lib/Route';
+import IndexRoute from 'react-router/lib/IndexRoute';
+import hashHistory from 'react-router/lib/hashHistory';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
@@ -7,10 +11,19 @@ import config from './reducers/config';
 import players from './reducers/players';
 import stopwatch from './reducers/stopwatch';
 
-import Header from './components/Header.jsx';
+import HeaderContainer from './components/Header.jsx';
 import ConfigContainer from './containers/ConfigContainer.jsx';
 import PlayerTimersContainer from './containers/PlayerTimersContainer.jsx';
 import TotalTimerContainer from './containers/TotalTimerContainer.jsx';
+
+function Index() {
+  return (
+    <div>
+      <PlayerTimersContainer />
+      <TotalTimerContainer />
+    </div>
+  );
+}
 
 const store = createStore(
   combineReducers({ config, players, stopwatch })
@@ -18,17 +31,12 @@ const store = createStore(
 
 render(
   <Provider store={store}>
-    <div>
-      <Header
-        title="Boardgame timer"
-      />
-
-      <div className="container container--pastNav">
-        <ConfigContainer />
-        <PlayerTimersContainer />
-        <TotalTimerContainer />
-      </div>
-    </div>
+    <Router history={hashHistory}>
+      <Route path="/" component={HeaderContainer} title="Boardgame timer">
+        <IndexRoute component={Index} />
+        <Route path="config" component={ConfigContainer} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('js-app')
 );
--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -1,29 +1,73 @@
 import React, { PropTypes } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
 
 const propTypes = {
+  activePath: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
 
-function Header(props) {
-  return (
-    <Navbar fixedTop>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">{props.title}</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">Config</NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+    this.state = {
+      expanded: false,
+    };
+
+    this.onNavbarToggle = this.onNavbarToggle.bind(this);
+    this.onNavItemClick = this.onNavItemClick.bind(this);
+  }
+
+  onNavbarToggle() {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  onNavItemClick() {
+    this.setState({ expanded: false });
+  }
+
+  render() {
+    return (
+      <Navbar
+        fixedTop
+        expanded={this.state.expanded}
+        onToggle={this.onNavbarToggle}
+      >
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">{this.props.title}</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav activeHref={`#${this.props.activePath}`}>
+            <NavItem onClick={this.onNavItemClick} href="#/">Timer</NavItem>
+            <NavItem onClick={this.onNavItemClick} href="#/config">Config</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 Header.propTypes = propTypes;
 
-export default Header;
+function HeaderContainer(props) {
+  return (
+    <div>
+      <Header
+        activePath={props.location.pathname}
+        title={props.route.title}
+      />
+
+      <div className="container container--pastNav">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default HeaderContainer;
