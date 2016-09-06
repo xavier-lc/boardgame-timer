7f1356a4fa5b1ad671de2b31ceb28201bec8042f
patillades <gorkamunya@hotmail.com>
2016-09-06 18:25:51 +0200

    show turn

--- lib/components/PlayerTimers.jsx
+++ lib/components/PlayerTimers.jsx
@@ -4,15 +4,22 @@ import classnames from 'classnames';
 import Player from './Player.jsx';
 import PlayerTotals from './PlayerTotals.jsx';
 
+import { getActivePlayerId } from './../reducers/players';
 import { timeToMs } from './../utils/date';
 
 function PlayerTimers(props) {
-  const timersCls = classnames({ hidden: props.stopwatch.finish !== null });
+  const timersCls = classnames('text-center', { hidden: props.stopwatch.finish !== null });
+  const turnCls = classnames({ hidden: props.stopwatch.start === null });
+
+  const id = getActivePlayerId(props.players);
+  const turn = id ? props.players.turns[id].length : 0;
 
   return (
     <div className="panel panel-primary">
       <div className="panel-body">
         <div className={timersCls}>
+          <h4 className={turnCls}>{`Turn ${turn}`}</h4>
+
           {props.players.ids.map(id => (
             <Player
               key={`player_${id}`}
