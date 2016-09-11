77daa028019cef9d75a07579fd74ca90f2d5788b
patillades <gorkamunya@hotmail.com>
2016-09-11 13:58:45 +0200

    player totals test

---
+++ tests/components/playerTotalsTest.jsx
@@ -0,0 +1,78 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { initialState as playersInitialState } from './../../lib/reducers/players';
+import { initialState as stopwatchInitialState } from './../../lib/reducers/stopwatch';
+import PlayerTotals from './../../lib/components/PlayerTotals.jsx';
+import TimerButton from './../../lib/components/TimerButton.jsx';
+
+/**
+ * Test props
+ *
+ * @param {?number} [start]
+ * @param {?number} [finish]
+ * @returns {{players, stopwatch: *}}
+ */
+function getProps(start = null, finish = null) {
+  const stopwatch = Object.assign({}, stopwatchInitialState, { start, finish });
+
+  return { players: playersInitialState, stopwatch };
+}
+
+/**
+ * Set up a PlayerTotals element for testing purposes
+ *
+ * @param {object} props
+ * @returns {ReactElement}
+ */
+function setup(props) {
+  const renderer = TestUtils.createRenderer();
+
+  renderer.render(<PlayerTotals {...props} />);
+
+  return renderer.getRenderOutput();
+}
+
+describe('PlayerTotals', function () {
+  it('should be hidden if timer has not started', function () {
+    const element = setup(getProps());
+    const button = element.props.children[0];
+    const totalsDiv = element.props.children[1];
+
+    expect(element.type).toBe('div');
+
+    expect(TestUtils.isElementOfType(button, TimerButton)).toBe(true);
+    expect(button.props.isVisible).toNotExist();
+
+    expect(totalsDiv.type).toBe('div');
+    expect(totalsDiv.props.className).toInclude('hidden');
+  });
+
+  it('should have as many totals as players has the state', function () {
+    const element = setup(getProps());
+    const totalsDiv = element.props.children[1];
+
+    expect(Array.isArray(totalsDiv.props.children)).toBe(true);
+    expect(totalsDiv.props.children.length).toBe(playersInitialState.ids.length);
+  });
+
+  it('should have button visible but not totals when timer starts', function () {
+    const element = setup(getProps(0));
+    const button = element.props.children[0];
+    const totalsDiv = element.props.children[1];
+
+    expect(button.props.isVisible).toExist();
+
+    expect(totalsDiv.props.className).toInclude('hidden');
+  });
+
+  it('should have totals visible but not button when timer finishes', function () {
+    const element = setup(getProps(0, 1));
+    const button = element.props.children[0];
+    const totalsDiv = element.props.children[1];
+
+    expect(button.props.isVisible).toNotExist();
+
+    expect(totalsDiv.props.className).toNotInclude('hidden');
+  });
+});
