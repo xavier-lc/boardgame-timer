fe3734f409f74b292f76a6d2507047115b3944f7
Xavier Lozano Carreras <xavier@mailtrack.io>
2019-05-20 17:39:57 +0200

    added watch:browserify grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,111 +1,116 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    browserify: {
-      dev: {
-        files: {
-          'src/app.js': 'lib/app.jsx',
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            dev: {
+                files: {
+                    'src/app.js': 'lib/app.jsx'
+                },
+                options: {
+                    transform: [
+                        ['loose-envify', { NODE_ENV: 'dev', global: true }],
+                        'babelify'
+                    ]
+                }
+            },
+            prod: {
+                files: {
+                    'src/app.js': 'lib/app.jsx'
+                },
+                options: {
+                    transform: [
+                        [
+                            'loose-envify',
+                            { NODE_ENV: 'production', global: true }
+                        ],
+                        'babelify'
+                    ]
+                }
+            }
         },
-        options: {
-          transform: [['loose-envify', { NODE_ENV: 'dev', global: true }], 'babelify'],
+        uglify: {
+            default: {
+                files: {
+                    'src/app.min.js': 'src/app.js'
+                }
+            }
         },
-      },
-      prod: {
-        files: {
-          'src/app.js': 'lib/app.jsx',
+        sass: {
+            default: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'expanded',
+                    noCache: true,
+                    trace: true
+                },
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
         },
-        options: {
-          transform: [['loose-envify', { NODE_ENV: 'production', global: true }], 'babelify'],
+        cssmin: {
+            options: {
+                report: 'min',
+                keepSpecialComments: 0
+            },
+            default: {
+                files: {
+                    'css/style.min.css': 'css/style.css'
+                }
+            }
         },
-      },
-    },
-    uglify: {
-      default: {
-        files: {
-          'src/app.min.js': 'src/app.js',
+        targethtml: {
+            dev: {
+                files: {
+                    'index.html': 'lib/index.html'
+                }
+            },
+            prod: {
+                files: {
+                    'index.html': 'lib/index.html'
+                }
+            }
         },
-      },
-    },
-    sass: {
-      default: {
-        options: {
-          sourcemap: 'none',
-          style: 'expanded',
-          noCache: true,
-          trace: true,
+        htmlmin: {
+            default: {
+                options: {
+                    collapseWhitespace: true,
+                    minifyJS: true
+                },
+                files: {
+                    'index.html': 'index.html'
+                }
+            }
         },
-        files: {
-          'css/style.css': 'css/style.scss',
-        },
-      },
-    },
-    cssmin: {
-      options: {
-        report: 'min',
-        keepSpecialComments: 0,
-      },
-      default: {
-        files: {
-          'css/style.min.css': 'css/style.css',
-        },
-      },
-    },
-    targethtml: {
-      dev: {
-        files: {
-          'index.html': 'lib/index.html',
-        },
-      },
-      prod: {
-        files: {
-          'index.html': 'lib/index.html',
-        },
-      },
-    },
-    htmlmin: {
-      default: {
-        options: {
-          collapseWhitespace: true,
-          minifyJS: true,
-        },
-        files: {
-          'index.html': 'index.html',
-        },
-      },
-    },
-    watch: {
-      files: ['lib/**/*'],
-      tasks: ['dev'],
-    },
-  });
+        watch: {
+            default: {
+                files: ['lib/**/*'],
+                tasks: ['dev']
+            },
+            browserify: {
+                files: ['lib/**/*'],
+                tasks: ['browserify:dev']
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-targethtml');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-targethtml');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['dev']);
+    grunt.registerTask('default', ['dev']);
 
-  grunt.registerTask(
-    'dev',
-    [
-      'browserify:dev',
-      'sass',
-      'targethtml:dev',
-    ]
-  );
+    grunt.registerTask('dev', ['browserify:dev', 'sass', 'targethtml:dev']);
 
-  grunt.registerTask(
-    'prod',
-    [
-      'browserify:prod',
-      'uglify',
-      'sass',
-      'cssmin',
-      'targethtml:prod',
-      'htmlmin',
-    ]
-  );
+    grunt.registerTask('prod', [
+        'browserify:prod',
+        'uglify',
+        'sass',
+        'cssmin',
+        'targethtml:prod',
+        'htmlmin'
+    ]);
 };
