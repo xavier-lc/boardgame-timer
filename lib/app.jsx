c3c827ef9fc314c27ddc131f7f597ed958f75aec
Xavier Lozano Carreras <xavier@mailtrack.io>
2019-05-22 14:58:36 +0200

    moved state to "state" folder

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,73 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import Route from 'react-router-dom/Route';
-import withRouter from 'react-router/withRouter';
 import HashRouter from 'react-router-dom/HashRouter';
-import { createStore, combineReducers } from 'redux';
 import Provider from 'react-redux/lib/components/Provider';
-
-import config from './reducers/config';
-import players from './reducers/players';
-import stopwatch from './reducers/stopwatch';
-
-import { pause, tick } from './actions/actions';
-
-import IndexContainer from './containers/IndexContainer.jsx';
-import ConfigContainer from './containers/ConfigContainer.jsx';
-import mapToLinkObj from './utils/mapToLinkObj';
-import LayoutContainerWithRouter from './containers/LayoutContainerWithRouter';
+import store from 'state/index';
+import LayoutContainerWithRouter from 'containers/LayoutContainerWithRouter';
+import mapToLinkObj from 'utils/mapToLinkObj';
+import ConfigContainer from 'containers/ConfigContainer';
+import IndexContainer from 'containers/IndexContainer';
 
 if (process.env.NODE_ENV === 'production') {
     ga('create', 'UA-83961876-1', 'auto');
 }
-
-const store = createStore(
-    combineReducers({
-        config,
-        players,
-        stopwatch
-    })
-);
-
-// keep track of the interval id being used to by the stopwatch's tick action
-let intervalId = null;
-
-store.subscribe(() => {
-    const state = store.getState();
-
-    if (state.stopwatch.isOn && !intervalId) {
-        return (intervalId = setInterval(() => store.dispatch(tick()), 100));
-    }
-
-    if (!state.stopwatch.isOn && intervalId) {
-        clearInterval(intervalId);
-
-        return (intervalId = null);
-    }
-});
-
-// TODO: withRouter
-// hashHistory.listen(location => {
-//   const state = store.getState();
-
-//   // whenever the user navigates out of the home page (where the timer is),
-//   // pause the stopwatch if it's running
-//   if (location.pathname !== '/' && state.stopwatch.isOn) {
-//     store.dispatch(pause());
-//   }
-
-//   if (process.env.NODE_ENV === 'production') {
-//     ga('set', 'page', location.pathname);
-//     ga('send', 'pageview');
-//   }
-// });
 
 const links = [
     ['/', 'Timer', IndexContainer],
     ['/config', 'Config', ConfigContainer]
 ].map(mapToLinkObj);
 
-render(
+ReactDOM.render(
     <Provider store={store}>
         <HashRouter>
             <LayoutContainerWithRouter
