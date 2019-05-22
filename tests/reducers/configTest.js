7863e51b25f39e90fcfb33ab8a12b13b2485f1c7
Xavier Lozano Carreras <xavier@mailtrack.io>
2019-05-22 15:30:13 +0200

    corrected test file imports

--- tests/reducers/configTest.js
+++ tests/reducers/configTest.js
@@ -1,18 +1,17 @@
 import expect from 'expect';
-import { TURN_TIME_CHANGE } from './../../lib/constants/actionTypes';
-import { changeTurnTime } from './../../lib/actions/actions';
-import config, { initialState } from './../../lib/reducers/config';
+import { changeTurnTime } from 'state/actions';
+import config, { initialState } from 'state/reducers/config';
 
-describe('config reducer', function () {
-  it('should update the player\'s turn time value', function () {
-    const minutes = 0;
-    let newState = config(initialState, changeTurnTime('minutes', minutes));
+describe('config reducer', function() {
+    it("should update the player's turn time value", function() {
+        const minutes = 0;
+        let newState = config(initialState, changeTurnTime('minutes', minutes));
 
-    expect(newState.turnTime.minutes).toBe(minutes);
+        expect(newState.turnTime.minutes).toBe(minutes);
 
-    const seconds = 30;
-    newState = config(initialState, changeTurnTime('seconds', seconds));
+        const seconds = 30;
+        newState = config(initialState, changeTurnTime('seconds', seconds));
 
-    expect(newState.turnTime.seconds).toBe(seconds);
-  });
+        expect(newState.turnTime.seconds).toBe(seconds);
+    });
 });
