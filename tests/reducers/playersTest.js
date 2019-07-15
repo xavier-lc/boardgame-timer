131a694c1ee5fc59a9b37a0e0a876632f2fd7d25
Xavier Lozano Carreras <xavier@mailtrack.io>
2019-07-15 12:34:12 +0200

    corrected imports

--- tests/reducers/playersTest.js
+++ tests/reducers/playersTest.js
@@ -1,173 +1,195 @@
 import expect from 'expect';
-import { ADD_PLAYER, PLAY, PAUSE, RESUME, TICK, NEXT } from './../../lib/constants/actionTypes';
-import { play, tick, pause, resume, stop, addPlayer, next, changeName }
-  from './../../lib/actions/actions';
-import players, { initialState } from './../../lib/reducers/players';
+import {
+    play,
+    tick,
+    pause,
+    resume,
+    addPlayer,
+    next,
+    changeName
+} from 'state/actions';
+import players, { initialState } from 'state/reducers/players';
 
-describe('players reducer', function () {
-  const initialLength = initialState.ids.length;
+describe('players reducer', function() {
+    const initialLength = initialState.ids.length;
 
-  it('should have 2 inactive players by default', function () {
-    expect(initialLength).toBe(2);
-    expect(Object.keys(initialState.data).length).toBe(2);
-    expect(Object.keys(initialState.offsets).length).toBe(2);
-    expect(Object.keys(initialState.turns).length).toBe(2);
+    it('should have 2 inactive players by default', function() {
+        expect(initialLength).toBe(2);
+        expect(Object.keys(initialState.data).length).toBe(2);
+        expect(Object.keys(initialState.offsets).length).toBe(2);
+        expect(Object.keys(initialState.turns).length).toBe(2);
 
-    expect(initialState.data[1].isActive).toBe(false);
-    expect(initialState.data[2].isActive).toBe(false);
+        expect(initialState.data[1].isActive).toBe(false);
+        expect(initialState.data[2].isActive).toBe(false);
 
-    expect(initialState.offsets[1]).toBe(null);
-    expect(initialState.offsets[2]).toBe(null);
-  });
+        expect(initialState.offsets[1]).toBe(null);
+        expect(initialState.offsets[2]).toBe(null);
+    });
 
-  it('should update user name on "CHANGE" action', function () {
-    const id = 1;
-    const name = 'tureru';
-    const newState = players(initialState, changeName(id, name));
+    it('should update user name on "CHANGE" action', function() {
+        const id = 1;
+        const name = 'tureru';
+        const newState = players(initialState, changeName(id, name));
 
-    expect(newState.data[id].name).toBe(name);
-  });
+        expect(newState.data[id].name).toBe(name);
+    });
 
-  it('should add a new player on "ADD_PLAYER" action', function () {
-    const newState = players(initialState, addPlayer());
+    it('should add a new player on "ADD_PLAYER" action', function() {
+        const newState = players(initialState, addPlayer());
 
-    expect(initialState.ids.length).toBe(
-      initialLength,
-      'The reducer is a pure function and shouldn\'t make changes to the initialState object'
-    );
+        expect(initialState.ids.length).toBe(
+            initialLength,
+            "The reducer is a pure function and shouldn't make changes to the initialState object"
+        );
 
-    expect(newState.ids.length).toBe(3);
-    expect(Object.keys(newState.data).length).toBe(3);
-    expect(Object.keys(newState.offsets).length).toBe(3);
-    expect(Object.keys(newState.turns).length).toBe(3);
-  });
+        expect(newState.ids.length).toBe(3);
+        expect(Object.keys(newState.data).length).toBe(3);
+        expect(Object.keys(newState.offsets).length).toBe(3);
+        expect(Object.keys(newState.turns).length).toBe(3);
+    });
 
-  context('PLAY/PAUSE/RESUME/TICK', function () {
-    let playAction;
-    let stateAfterPlay;
+    context('PLAY/PAUSE/RESUME/TICK', function() {
+        let playAction;
+        let stateAfterPlay;
 
-    let pauseAction;
-    let stateAfterPause;
+        let pauseAction;
+        let stateAfterPause;
 
-    let resumeAction;
-    let stateAfterResume;
+        let resumeAction;
+        let stateAfterResume;
 
-    let tickAction;
-    let stateAfterTick;
+        let tickAction;
+        let stateAfterTick;
 
-    let nextAction;
-    let stateAfterNext;
+        let nextAction;
+        let stateAfterNext;
 
-    let secondNextAction;
-    let stateAfterSecondNext;
+        let secondNextAction;
+        let stateAfterSecondNext;
 
-    before(function (done) {
-      playAction = play();
-      stateAfterPlay = players(initialState, playAction);
+        before(function(done) {
+            playAction = play();
+            stateAfterPlay = players(initialState, playAction);
 
-      // make sure that there's a bit of time between the actions
-      setTimeout(function () {
-        pauseAction = pause();
-        stateAfterPause = players(stateAfterPlay, pauseAction);
+            // make sure that there's a bit of time between the actions
+            setTimeout(function() {
+                pauseAction = pause();
+                stateAfterPause = players(stateAfterPlay, pauseAction);
 
-        setTimeout(function () {
-          resumeAction = resume();
-          stateAfterResume = players(stateAfterPause, resumeAction);
+                setTimeout(function() {
+                    resumeAction = resume();
+                    stateAfterResume = players(stateAfterPause, resumeAction);
 
-          setTimeout(function () {
-            tickAction = tick();
-            stateAfterTick = players(stateAfterResume, tickAction);
+                    setTimeout(function() {
+                        tickAction = tick();
+                        stateAfterTick = players(stateAfterResume, tickAction);
 
-            setTimeout(function () {
-              nextAction = next();
-              stateAfterNext = players(stateAfterTick, nextAction);
+                        setTimeout(function() {
+                            nextAction = next();
+                            stateAfterNext = players(
+                                stateAfterTick,
+                                nextAction
+                            );
 
-              setTimeout(function () {
-                secondNextAction = next();
-                stateAfterSecondNext = players(stateAfterNext, secondNextAction);
+                            setTimeout(function() {
+                                secondNextAction = next();
+                                stateAfterSecondNext = players(
+                                    stateAfterNext,
+                                    secondNextAction
+                                );
 
-                done();
-              }, 100);
+                                done();
+                            }, 100);
+                        }, 100);
+                    }, 100);
+                }, 100);
             }, 100);
-          }, 100);
-        }, 100);
-      }, 100);
+        });
+
+        it('should set player #1 as active on "PLAY" action', function() {
+            expect(stateAfterPlay.data[1].isActive).toBe(true);
+            expect(stateAfterPlay.data[2].isActive).toBe(false);
+
+            expect(stateAfterPlay.offsets[1]).toBe(playAction.time);
+            expect(stateAfterPlay.offsets[2]).toBe(null);
+
+            expect(stateAfterPlay.turns[1].length).toBe(1);
+            expect(stateAfterPlay.turns[2].length).toBe(0);
+
+            expect(initialState.offsets[1]).toBe(
+                null,
+                "The reducer is a pure function and shouldn't make changes to the initialState object"
+            );
+        });
+
+        it('should keep track of elapsed time on "PAUSE" action', function() {
+            expect(stateAfterPause.offsets[1]).toBe(
+                stateAfterPlay.offsets[1],
+                'The PAUSE action has nothing to do with the offset prop'
+            );
+
+            expect(stateAfterPause.turns[1][0]).toBe(
+                pauseAction.time - stateAfterPause.offsets[1]
+            );
+
+            expect(stateAfterPause.turns[1][0]).toBeMoreThan(
+                stateAfterPlay.turns[1][0]
+            );
+        });
+
+        it('should update player\'s offset on "RESUME" action', function() {
+            expect(stateAfterResume.offsets[1]).toBe(
+                resumeAction.time - stateAfterPause.turns[1][0]
+            );
+            expect(stateAfterResume.offsets[1]).toBeMoreThan(
+                stateAfterPause.offsets[1]
+            );
+        });
+
+        it('should update player\'s time on "TICK" action', function() {
+            expect(stateAfterTick.turns[1][0]).toBe(
+                tickAction.time - stateAfterResume.offsets[1]
+            );
+
+            expect(stateAfterTick.turns[1][0]).toBeMoreThan(
+                stateAfterResume.turns[1][0]
+            );
+        });
+
+        it('should update player\'s time on "NEXT" action and set the next one as active', function() {
+            expect(stateAfterNext.turns[1][0]).toBe(
+                nextAction.time - stateAfterTick.offsets[1]
+            );
+
+            expect(stateAfterNext.turns[1][0]).toBeMoreThan(
+                stateAfterTick.turns[1][0]
+            );
+
+            expect(stateAfterNext.data[1].isActive).toBe(false);
+            expect(stateAfterNext.data[2].isActive).toBe(true);
+
+            expect(stateAfterNext.offsets[2]).toBe(nextAction.time);
+
+            expect(stateAfterNext.turns[1].length).toBe(1);
+            expect(stateAfterNext.turns[2].length).toBe(1);
+        });
+
+        it('should go back to player #1 on another "NEXT" action', function() {
+            expect(stateAfterSecondNext.turns[2][0]).toBe(
+                secondNextAction.time - stateAfterNext.offsets[2]
+            );
+
+            expect(stateAfterSecondNext.turns[2][0]).toBeMoreThan(
+                stateAfterNext.turns[2][0]
+            );
+
+            expect(stateAfterSecondNext.data[1].isActive).toBe(true);
+            expect(stateAfterSecondNext.data[2].isActive).toBe(false);
+
+            expect(stateAfterSecondNext.offsets[1]).toBe(secondNextAction.time);
+
+            expect(stateAfterSecondNext.turns[1].length).toBe(2);
+            expect(stateAfterSecondNext.turns[2].length).toBe(1);
+        });
     });
-
-    it('should set player #1 as active on "PLAY" action', function () {
-      expect(stateAfterPlay.data[1].isActive).toBe(true);
-      expect(stateAfterPlay.data[2].isActive).toBe(false);
-
-      expect(stateAfterPlay.offsets[1]).toBe(playAction.time);
-      expect(stateAfterPlay.offsets[2]).toBe(null);
-
-      expect(stateAfterPlay.turns[1].length).toBe(1);
-      expect(stateAfterPlay.turns[2].length).toBe(0);
-
-      expect(initialState.offsets[1]).toBe(
-        null,
-        'The reducer is a pure function and shouldn\'t make changes to the initialState object'
-      );
-    });
-
-    it('should keep track of elapsed time on "PAUSE" action', function () {
-      expect(stateAfterPause.offsets[1]).toBe(
-        stateAfterPlay.offsets[1],
-        'The PAUSE action has nothing to do with the offset prop'
-      );
-
-      expect(stateAfterPause.turns[1][0]).toBe(
-        pauseAction.time - stateAfterPause.offsets[1]
-      );
-
-      expect(stateAfterPause.turns[1][0]).toBeMoreThan(stateAfterPlay.turns[1][0]);
-    });
-
-    it('should update player\'s offset on "RESUME" action', function () {
-      expect(stateAfterResume.offsets[1]).toBe(
-        resumeAction.time - stateAfterPause.turns[1][0]
-      );
-      expect(stateAfterResume.offsets[1]).toBeMoreThan(stateAfterPause.offsets[1]);
-    });
-
-    it('should update player\'s time on "TICK" action', function () {
-      expect(stateAfterTick.turns[1][0]).toBe(
-        tickAction.time - stateAfterResume.offsets[1]
-      );
-
-      expect(stateAfterTick.turns[1][0]).toBeMoreThan(stateAfterResume.turns[1][0]);
-    });
-
-    it('should update player\'s time on "NEXT" action and set the next one as active', function () {
-      expect(stateAfterNext.turns[1][0]).toBe(
-        nextAction.time - stateAfterTick.offsets[1]
-      );
-
-      expect(stateAfterNext.turns[1][0]).toBeMoreThan(stateAfterTick.turns[1][0]);
-
-      expect(stateAfterNext.data[1].isActive).toBe(false);
-      expect(stateAfterNext.data[2].isActive).toBe(true);
-
-      expect(stateAfterNext.offsets[2]).toBe(nextAction.time);
-
-      expect(stateAfterNext.turns[1].length).toBe(1);
-      expect(stateAfterNext.turns[2].length).toBe(1);
-    });
-
-    it('should go back to player #1 on another "NEXT" action', function () {
-      expect(stateAfterSecondNext.turns[2][0]).toBe(
-        secondNextAction.time - stateAfterNext.offsets[2]
-      );
-
-      expect(stateAfterSecondNext.turns[2][0]).toBeMoreThan(stateAfterNext.turns[2][0]);
-
-      expect(stateAfterSecondNext.data[1].isActive).toBe(true);
-      expect(stateAfterSecondNext.data[2].isActive).toBe(false);
-
-      expect(stateAfterSecondNext.offsets[1]).toBe(secondNextAction.time);
-
-      expect(stateAfterSecondNext.turns[1].length).toBe(2);
-      expect(stateAfterSecondNext.turns[2].length).toBe(1);
-    });
-  });
 });
--- tests/reducers/stopwatchTest.js
+++ tests/reducers/stopwatchTest.js
@@ -1,111 +1,121 @@
 import expect from 'expect';
-import { ADD_PLAYER, PLAY, PAUSE, RESUME, TICK, NEXT } from './../../lib/constants/actionTypes';
-import { play, tick, pause, resume, stop, addPlayer, next } from './../../lib/actions/actions';
-import stopwatch, { initialState } from './../../lib/reducers/stopwatch';
-import { msToTime } from './../../lib/utils/date';
+import { play, tick, pause, resume, stop } from 'state/actions';
+import stopwatch, { initialState } from 'state/reducers/stopwatch';
+import { msToTime } from 'utils/date';
 
-describe('stopwatch reducer', function () {
-  it('should be off on its initial state', function () {
-    expect(initialState.isOn).toBe(false);
-    expect(initialState.start).toBe(null);
-    expect(initialState.offset).toBe(null);
-    expect(initialState.elapsed).toBe(0);
-    expect(initialState.finish).toBe(null);
-  });
-
-  context('PLAY/PAUSE/RESUME/TICK', function () {
-    let playAction;
-    let stateAfterPlay;
-
-    let pauseAction;
-    let stateAfterPause;
-
-    let resumeAction;
-    let stateAfterResume;
-
-    let tickAction;
-    let stateAfterTick;
-
-    let secondPauseAction;
-    let stateAfterSecondPause;
-
-    let stopAction;
-    let stateAfterStop;
-
-    before(function (done) {
-      playAction = play();
-      stateAfterPlay = stopwatch(initialState, playAction);
-
-      // make sure that there's a bit of time between the actions
-      setTimeout(function () {
-        pauseAction = pause();
-        stateAfterPause = stopwatch(stateAfterPlay, pauseAction);
-
-        setTimeout(function () {
-          resumeAction = resume();
-          stateAfterResume = stopwatch(stateAfterPause, resumeAction);
-
-          setTimeout(function () {
-            tickAction = tick();
-            stateAfterTick = stopwatch(stateAfterResume, tickAction);
-
-            setTimeout(function () {
-              secondPauseAction = pause();
-              stateAfterSecondPause = stopwatch(stateAfterTick, secondPauseAction);
-
-              stopAction = stop();
-              stateAfterStop = stopwatch(stateAfterSecondPause, stopAction);
-
-              done();
-            }, 100);
-          }, 100);
-        }, 100);
-      }, 1100);
+describe('stopwatch reducer', function() {
+    it('should be off on its initial state', function() {
+        expect(initialState.isOn).toBe(false);
+        expect(initialState.start).toBe(null);
+        expect(initialState.offset).toBe(null);
+        expect(initialState.elapsed).toBe(0);
+        expect(initialState.finish).toBe(null);
     });
 
-    it('should be started on "PLAY" action', function () {
-      expect(stateAfterPlay.isOn).toBe(true);
-      expect(stateAfterPlay.start).toBe(playAction.time);
-      expect(stateAfterPlay.offset).toBe(playAction.time);
+    context('PLAY/PAUSE/RESUME/TICK', function() {
+        let playAction;
+        let stateAfterPlay;
+
+        let pauseAction;
+        let stateAfterPause;
+
+        let resumeAction;
+        let stateAfterResume;
+
+        let tickAction;
+        let stateAfterTick;
+
+        let secondPauseAction;
+        let stateAfterSecondPause;
+
+        let stopAction;
+        let stateAfterStop;
+
+        before(function(done) {
+            playAction = play();
+            stateAfterPlay = stopwatch(initialState, playAction);
+
+            // make sure that there's a bit of time between the actions
+            setTimeout(function() {
+                pauseAction = pause();
+                stateAfterPause = stopwatch(stateAfterPlay, pauseAction);
+
+                setTimeout(function() {
+                    resumeAction = resume();
+                    stateAfterResume = stopwatch(stateAfterPause, resumeAction);
+
+                    setTimeout(function() {
+                        tickAction = tick();
+                        stateAfterTick = stopwatch(
+                            stateAfterResume,
+                            tickAction
+                        );
+
+                        setTimeout(function() {
+                            secondPauseAction = pause();
+                            stateAfterSecondPause = stopwatch(
+                                stateAfterTick,
+                                secondPauseAction
+                            );
+
+                            stopAction = stop();
+                            stateAfterStop = stopwatch(
+                                stateAfterSecondPause,
+                                stopAction
+                            );
+
+                            done();
+                        }, 100);
+                    }, 100);
+                }, 100);
+            }, 1100);
+        });
+
+        it('should be started on "PLAY" action', function() {
+            expect(stateAfterPlay.isOn).toBe(true);
+            expect(stateAfterPlay.start).toBe(playAction.time);
+            expect(stateAfterPlay.offset).toBe(playAction.time);
+        });
+
+        it('should be updated on "PAUSE" action and 1 second should be elapsed', function() {
+            const elapsed = pauseAction.time - stateAfterPlay.offset;
+
+            expect(stateAfterPause).toEqual(
+                Object.assign({}, stateAfterPlay, {
+                    isOn: false,
+                    elapsed
+                })
+            );
+
+            const time = msToTime(elapsed);
+
+            expect(time.seconds).toBe(1);
+        });
+
+        it('should be back "on" on "RESUME" action and no time should have gone by', function() {
+            expect(stateAfterResume.isOn).toBe(true);
+            expect(stateAfterResume.elapsed).toBe(stateAfterPause.elapsed);
+            expect(stateAfterResume.offset).toBe(
+                resumeAction.time - stateAfterPause.elapsed
+            );
+        });
+
+        it('should be updated on "TICK" action', function() {
+            expect(stateAfterTick).toEqual(
+                Object.assign({}, stateAfterResume, {
+                    elapsed: tickAction.time - stateAfterResume.offset
+                })
+            );
+        });
+
+        it('should finish on "STOP" action', function() {
+            expect(stateAfterStop).toEqual(
+                Object.assign({}, stateAfterTick, {
+                    isOn: false,
+                    elapsed: secondPauseAction.time - stateAfterTick.offset,
+                    finish: stopAction.time
+                })
+            );
+        });
     });
-
-    it('should be updated on "PAUSE" action and 1 second should be elapsed', function () {
-      const elapsed = pauseAction.time - stateAfterPlay.offset;
-
-      expect(stateAfterPause).toEqual(
-        Object.assign({}, stateAfterPlay, {
-          isOn: false,
-          elapsed,
-        })
-      );
-
-      const time = msToTime(elapsed);
-
-      expect(time.seconds).toBe(1);
-    });
-
-    it('should be back "on" on "RESUME" action and no time should have gone by', function () {
-      expect(stateAfterResume.isOn).toBe(true);
-      expect(stateAfterResume.elapsed).toBe(stateAfterPause.elapsed);
-      expect(stateAfterResume.offset).toBe(resumeAction.time - stateAfterPause.elapsed);
-    });
-
-    it('should be updated on "TICK" action', function () {
-      expect(stateAfterTick).toEqual(
-        Object.assign({}, stateAfterResume, {
-          elapsed: tickAction.time - stateAfterResume.offset,
-        })
-      );
-    });
-
-    it('should finish on "STOP" action', function () {
-      expect(stateAfterStop).toEqual(
-        Object.assign({}, stateAfterTick, {
-          isOn: false,
-          elapsed: secondPauseAction.time - stateAfterTick.offset,
-          finish: stopAction.time,
-        })
-      );
-    });
-  });
 });
